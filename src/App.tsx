11af1e7395b4c104b990af5a770b54d90e7c9c7a
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-05-26 15:33:33 +0100

    fetching posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './App.css';
+import { usePostsQuery } from './services/postsApi'
 
 function App() {
+  const { data , error, isLoading, isFetching, isSuccess } = usePostsQuery();
+
   return (
     <div className="App">
-      HHHHHHHHHH
+      <h1>React Redux Toolkit RTK Query Tutorial</h1>
+      {isLoading && <p>Loading...</p>}
+      {isFetching && <p>Fetching...</p>}
+      {error && <p>something went wrong</p>}
+      {isSuccess && 
+      (<div>
+        {
+          data.map(post => {
+          return  <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                  </div>
+          })
+        }
+
+      </div>)
+      }
     </div>
   );
 }
